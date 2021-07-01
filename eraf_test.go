7e3d9592957976b8e123c44b2d7c57a6dbed973c
Robin Kaiser <web@r-k.mx>
2021-07-01 20:28:38 +0200

    working on the tests

--- eraf_test.go
+++ eraf_test.go
@@ -1,3 +1,65 @@
 package era
 
-// re-create proper tests
+import (
+	"bytes"
+	"io"
+	"os"
+	"testing"
+)
+
+func BenchmarkUnmarshal(b *testing.B) {
+	var (
+		start = New()
+		buf bytes.Buffer
+		err error
+		container = New()
+	)
+
+	err = start.Marshal(&buf)
+	if err != nil {
+		b.Fatal(err.Error())
+	}
+
+	for i := 0; i < b.N; i++ {
+		err = Unmarshal(&buf, container)
+		if err != nil {
+			b.Error(err.Error())
+		}
+	}
+}
+
+func BenchmarkMarshal(b *testing.B) {
+	var (
+		container = New()
+		err error
+	)
+	for i := 0; i < b.N; i++ {
+		err = container.Marshal(io.Discard)
+		if err != nil {
+			b.Error(err.Error())
+		}
+	}
+}
+
+func BenchmarkMarshalToFile(b *testing.B) {
+	var (
+		container = New()
+		err error
+	)
+	fh, err := os.Create("__test.eraf")
+	if err != nil {
+		b.Fatal(err.Error())
+	}
+
+	defer func() {
+		_ = os.Remove("__test.eraf")
+	}()
+	defer fh.Close()
+
+	for i := 0; i < b.N; i++ {
+		err = container.Marshal(fh)
+		if err != nil {
+			b.Errorf(err.Error())
+		}
+	}
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/KaiserWerk/VHOK-Go-SDK
+module github.com/KaiserWerk/ERAF-Go-SDK
 
 go 1.16
